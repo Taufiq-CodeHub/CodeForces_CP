d2f3e833fe36f144ee141148a9048a9aa38d37a4
Taufiq-CodeBase <taufiq.connect+github@gmail.com>
2023-09-25 19:42:55 +0600

    test commit 1

--- 006ChipsBoard.cpp
+++ 006ChipsBoard.cpp
@@ -33,6 +33,7 @@ int main(){
             }
         }
         
+        cout<<a[0]<<endl;
 
         cout<<a_mn<<" "<<a_mn_indx<<endl<<b_mn<<" "<<b_mn_indx<<endl;
         // for (int i = 0; i < n; i++)
