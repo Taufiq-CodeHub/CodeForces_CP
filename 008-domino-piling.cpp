729ef8253c90bf76d92fa846cb3842a7bbb91063
Taufiq-CodeBase <taufiq.connect+github@gmail.com>
2023-11-04 16:41:57 +0600

    cp

---
+++ 008-domino-piling.cpp
@@ -0,0 +1,12 @@
+//https://codeforces.com/problemset/problem/50/A
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a,b,ans;
+    cin>>a>>b;
+    cout<<(b*a)/2<<endl;
+
+    return 0;
+}
